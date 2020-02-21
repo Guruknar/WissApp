136db6a6fa7676b163a2a95d735cdc8dcf2761df
Section2 <Section2@DESKTOP-KHOPMPR>
2020-02-21 21:16:18 +0300

    Add project files.

---
+++ AppCore/Repository/Base/RepositoryBase.cs
@@ -0,0 +1,51 @@
+﻿using AppCore.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Repository.Base
+{
+    public abstract class RepositoryBase<TEntity> : IDisposable where TEntity : class, new()
+    {
+        #region Dispose,garbagecollector
+        protected bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+        protected IDBcontext db;
+        //protected DbContext db;
+        public RepositoryBase(IDBcontext _db)
+        {
+            db = _db;
+        }
+
+        public virtual List<TEntity> GetEntities()
+        {
+            try
+            {
+                return db.Set<TEntity>().ToList();
+            }
+            catch (Exception exc)
+            {
+
+                throw exc;
+            }
+            
+        }
+
+
+
+    }
+}
---
+++ WissAppEntities/Entities/Users.cs
@@ -0,0 +1,56 @@
+namespace WissAppEntities.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public partial class Users
+    {
+     
+        public Users()
+        {
+            SenderMessages = new HashSet<UsersMessages>();
+            ReceiverMessages = new HashSet<UsersMessages>();
+        }
+
+        public int Id { get; set; }
+
+        public int RoleId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Password { get; set; }
+
+        [Column("E-Mail")]
+        [Required]
+        [MaxLength(200)]
+        public byte[] E_Mail { get; set; }
+
+        [StringLength(300)]
+        public string School { get; set; }
+
+        [StringLength(150)]
+        public string Location { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? BirthDate { get; set; }
+
+        [StringLength(1)]
+        public string Gender { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public virtual Roles Roles { get; set; }
+
+
+        public virtual ICollection<UsersMessages> SenderMessages { get; set; }
+
+   
+        public virtual ICollection<UsersMessages> ReceiverMessages { get; set; }
+    }
+}
