8f90524c5a89581950fb5b7931ff2a701410e53a
Section2 <Section2@DESKTOP-KHOPMPR>
2020-02-26 18:29:04 +0300

    comtests

---
+++ WissAppWebApi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WissAppWebApi.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WissAppWebApi.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("76b64be3-b073-44f2-bdda-1d6b04a35b5c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WissAppWebApi.Tests/WissAppTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+using AppCore.Services;
+using AppCore.Services.Base;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WissAppEF.Contexts;
+using WissAppEntities.Entities;
+
+namespace WissAppWebApi.Tests
+{
+    [TestClass]
+    public class WissAppTest
+    {
+        DbContext db = new WissAppContext();
+        [TestMethod]
+        public void ShouldGet4users()
+        {
+            using (var service = new Service<Users>(db))
+            {
+                var entities = service.GetEntities();
+                Assert.AreEqual(4, entities.Count);
+            }
+        }
+    }
+}
